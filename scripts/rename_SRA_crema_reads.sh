c38f6a4e8d86113cee5a2624c800da4d513766d0
for-giobbe <forni.giobbe@gmail.com>
2021-09-14 21:53:45 +0200

    new

---
+++ scripts/rename_SRA_crema_reads.sh
@@ -0,0 +1,102 @@
+cd $1
+mkdir ../crema_ref
+
+# mate 1
+
+ln S-A1-A_1.fastq.gz ../crema_ref/CR_A_01_AD_1.fastq.gz
+ln S-A2-A_1.fastq.gz ../crema_ref/CR_A_02_AD_1.fastq.gz
+ln S-A3-A_1.fastq.gz ../crema_ref/CR_A_03_AD_1.fastq.gz
+ln S-A5-A_1.fastq.gz ../crema_ref/CR_A_05_AD_1.fastq.gz
+ln S-A6-A_1.fastq.gz ../crema_ref/CR_A_06_AD_1.fastq.gz
+
+ln S-A1-CT_1.fastq.gz ../crema_ref/CR_A_01_CT_1.fastq.gz
+ln S-A2-CT_1.fastq.gz ../crema_ref/CR_A_02_CT_1.fastq.gz
+ln S-A3-CT_1.fastq.gz ../crema_ref/CR_A_03_CT_1.fastq.gz
+ln S-A5-CT_1.fastq.gz ../crema_ref/CR_A_05_CT_1.fastq.gz
+ln S-A6-CT_1.fastq.gz ../crema_ref/CR_A_06_CT_1.fastq.gz
+
+ln S-B1-A_1.fastq.gz ../crema_ref/CR_B_01_AD_1.fastq.gz
+ln S-B2-A_1.fastq.gz ../crema_ref/CR_B_02_AD_1.fastq.gz
+ln S-B3-A_1.fastq.gz ../crema_ref/CR_B_03_AD_1.fastq.gz
+ln S-B4-A_1.fastq.gz ../crema_ref/CR_B_04_AD_1.fastq.gz
+ln S-B5-A_1.fastq.gz ../crema_ref/CR_B_05_AD_1.fastq.gz
+
+ln S-B1-CT_1.fastq.gz ../crema_ref/CR_B_01_CT_1.fastq.gz
+ln S-B2-CT_1.fastq.gz ../crema_ref/CR_B_02_CT_1.fastq.gz
+ln S-B3-CT_1.fastq.gz ../crema_ref/CR_B_03_CT_1.fastq.gz
+ln S-B4-CT_1.fastq.gz ../crema_ref/CR_B_04_CT_1.fastq.gz
+ln S-B5-CT_1.fastq.gz ../crema_ref/CR_B_05_CT_1.fastq.gz
+
+ln S-C2-A_1.fastq.gz ../crema_ref/CR_C_02_AD_1.fastq.gz
+ln S-C3-A_1.fastq.gz ../crema_ref/CR_C_03_AD_1.fastq.gz
+ln S-C6-A_1.fastq.gz ../crema_ref/CR_C_06_AD_1.fastq.gz
+ln S-C7-A_1.fastq.gz ../crema_ref/CR_C_07_AD_1.fastq.gz
+ln S-C8-A_1.fastq.gz ../crema_ref/CR_C_08_AD_1.fastq.gz
+
+ln S-C2-CT_1.fastq.gz ../crema_ref/CR_C_02_CT_1.fastq.gz
+ln S-C3-CT_1.fastq.gz ../crema_ref/CR_C_03_CT_1.fastq.gz
+ln S-C6-CT_1.fastq.gz ../crema_ref/CR_C_06_CT_1.fastq.gz
+ln S-C7-CT_1.fastq.gz ../crema_ref/CR_C_07_CT_1.fastq.gz
+ln S-C8-CT_1.fastq.gz ../crema_ref/CR_C_08_CT_1.fastq.gz
+
+ln S-D1-A_1.fastq.gz ../crema_ref/CR_D_01_AD_1.fastq.gz
+ln S-D2-A_1.fastq.gz ../crema_ref/CR_D_02_AD_1.fastq.gz
+ln S-D3-A_1.fastq.gz ../crema_ref/CR_D_03_AD_1.fastq.gz
+ln S-D4-A_1.fastq.gz ../crema_ref/CR_D_04_AD_1.fastq.gz
+ln S-D6-A_1.fastq.gz ../crema_ref/CR_D_06_AD_1.fastq.gz
+
+ln S-D1-CT_1.fastq.gz ../crema_ref/CR_D_01_CT_1.fastq.gz
+ln S-D2-CT_1.fastq.gz ../crema_ref/CR_D_02_CT_1.fastq.gz
+ln S-D3-CT_1.fastq.gz ../crema_ref/CR_D_03_CT_1.fastq.gz
+ln S-D4-CT_1.fastq.gz ../crema_ref/CR_D_04_CT_1.fastq.gz
+ln S-D6-CT_1.fastq.gz ../crema_ref/CR_D_06_CT_1.fastq.gz
+
+# mate 2
+
+ln S-A1-A_2.fastq.gz ../crema_ref/CR_A_01_AD_2.fastq.gz
+ln S-A2-A_2.fastq.gz ../crema_ref/CR_A_02_AD_2.fastq.gz
+ln S-A3-A_2.fastq.gz ../crema_ref/CR_A_03_AD_2.fastq.gz
+ln S-A5-A_2.fastq.gz ../crema_ref/CR_A_05_AD_2.fastq.gz
+ln S-A6-A_2.fastq.gz ../crema_ref/CR_A_06_AD_2.fastq.gz
+
+ln S-A1-CT_2.fastq.gz ../crema_ref/CR_A_01_CT_2.fastq.gz
+ln S-A2-CT_2.fastq.gz ../crema_ref/CR_A_02_CT_2.fastq.gz
+ln S-A3-CT_2.fastq.gz ../crema_ref/CR_A_03_CT_2.fastq.gz
+ln S-A5-CT_2.fastq.gz ../crema_ref/CR_A_05_CT_2.fastq.gz
+ln S-A6-CT_2.fastq.gz ../crema_ref/CR_A_06_CT_2.fastq.gz
+
+ln S-B1-A_2.fastq.gz ../crema_ref/CR_B_01_AD_2.fastq.gz
+ln S-B2-A_2.fastq.gz ../crema_ref/CR_B_02_AD_2.fastq.gz
+ln S-B3-A_2.fastq.gz ../crema_ref/CR_B_03_AD_2.fastq.gz
+ln S-B4-A_2.fastq.gz ../crema_ref/CR_B_04_AD_2.fastq.gz
+ln S-B5-A_2.fastq.gz ../crema_ref/CR_B_05_AD_2.fastq.gz
+
+ln S-B1-CT_2.fastq.gz ../crema_ref/CR_B_01_CT_2.fastq.gz
+ln S-B2-CT_2.fastq.gz ../crema_ref/CR_B_02_CT_2.fastq.gz
+ln S-B3-CT_2.fastq.gz ../crema_ref/CR_B_03_CT_2.fastq.gz
+ln S-B4-CT_2.fastq.gz ../crema_ref/CR_B_04_CT_2.fastq.gz
+ln S-B5-CT_2.fastq.gz ../crema_ref/CR_B_05_CT_2.fastq.gz
+
+ln S-C2-A_2.fastq.gz ../crema_ref/CR_C_02_AD_2.fastq.gz
+ln S-C3-A_2.fastq.gz ../crema_ref/CR_C_03_AD_2.fastq.gz
+ln S-C6-A_2.fastq.gz ../crema_ref/CR_C_06_AD_2.fastq.gz
+ln S-C7-A_2.fastq.gz ../crema_ref/CR_C_07_AD_2.fastq.gz
+ln S-C8-A_2.fastq.gz ../crema_ref/CR_C_08_AD_2.fastq.gz
+
+ln S-C2-CT_2.fastq.gz ../crema_ref/CR_C_02_CT_2.fastq.gz
+ln S-C3-CT_2.fastq.gz ../crema_ref/CR_C_03_CT_2.fastq.gz
+ln S-C6-CT_2.fastq.gz ../crema_ref/CR_C_06_CT_2.fastq.gz
+ln S-C7-CT_2.fastq.gz ../crema_ref/CR_C_07_CT_2.fastq.gz
+ln S-C8-CT_2.fastq.gz ../crema_ref/CR_C_08_CT_2.fastq.gz
+
+ln S-D1-A_2.fastq.gz ../crema_ref/CR_D_01_AD_2.fastq.gz
+ln S-D2-A_2.fastq.gz ../crema_ref/CR_D_02_AD_2.fastq.gz
+ln S-D3-A_2.fastq.gz ../crema_ref/CR_D_03_AD_2.fastq.gz
+ln S-D4-A_2.fastq.gz ../crema_ref/CR_D_04_AD_2.fastq.gz
+ln S-D6-A_2.fastq.gz ../crema_ref/CR_D_06_AD_2.fastq.gz
+
+ln S-D1-CT_2.fastq.gz ../crema_ref/CR_D_01_CT_2.fastq.gz
+ln S-D2-CT_2.fastq.gz ../crema_ref/CR_D_02_CT_2.fastq.gz
+ln S-D3-CT_2.fastq.gz ../crema_ref/CR_D_03_CT_2.fastq.gz
+ln S-D4-CT_2.fastq.gz ../crema_ref/CR_D_04_CT_2.fastq.gz
+ln S-D6-CT_2.fastq.gz ../crema_ref/CR_D_06_CT_2.fastq.gz
---
+++ scripts/rename_SRA_vicia_reads.sh
@@ -0,0 +1,29 @@
+cd $1
+mkdir ../vicia_ref
+
+# mate 1
+
+ln SRR15671608_1.fastq ../vicia_ref/VI_N_20_NP_1.fastq.gz
+ln SRR15671607_1.fastq ../vicia_ref/VI_N_21_NP_1.fastq.gz
+ln SRR15671606_1.fastq ../vicia_ref/VI_N_22_NP_1.fastq.gz
+ln SRR15671605_1.fastq ../vicia_ref/VI_N_23_NP_1.fastq.gz
+ln SRR15671604_1.fastq ../vicia_ref/VI_N_24_NP_1.fastq.gz
+ln SRR15671603_1.fastq ../vicia_ref/VI_V_26_NP_1.fastq.gz
+ln SRR15671602_1.fastq ../vicia_ref/VI_V_27_NP_1.fastq.gz
+ln SRR15671601_1.fastq ../vicia_ref/VI_V_28_NP_1.fastq.gz
+ln SRR15671600_1.fastq ../vicia_ref/VI_V_30_NP_1.fastq.gz
+ln SRR15671599_1.fastq ../vicia_ref/VI_V_31_NP_1.fastq.gz
+
+# mate 2
+
+ln SRR15671608_2.fastq ../vicia_ref/VI_N_20_NP_2.fastq.gz
+ln SRR15671607_2.fastq ../vicia_ref/VI_N_21_NP_2.fastq.gz
+ln SRR15671606_2.fastq ../vicia_ref/VI_N_22_NP_2.fastq.gz
+ln SRR15671605_2.fastq ../vicia_ref/VI_N_23_NP_2.fastq.gz
+ln SRR15671604_2.fastq ../vicia_ref/VI_N_24_NP_2.fastq.gz
+ln SRR15671603_2.fastq ../vicia_ref/VI_V_26_NP_2.fastq.gz
+ln SRR15671602_2.fastq ../vicia_ref/VI_V_27_NP_2.fastq.gz
+ln SRR15671601_2.fastq ../vicia_ref/VI_V_28_NP_2.fastq.gz
+ln SRR15671600_2.fastq ../vicia_ref/VI_V_30_NP_2.fastq.gz
+ln SRR15671599_2.fastq ../vicia_ref/VI_V_31_NP_2.fastq.gz
+
